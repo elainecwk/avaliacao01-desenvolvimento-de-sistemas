26721cbb84aa01d41ce1393d58a029f53b6f6a8b
ELAINE CRISTINA DE AMORIM <elaine_c_amorim@estudante.sesisenai.org.br>
2025-02-18 21:48:35 -0300

    avaliacao

--- src/atividade06.ts
+++ src/atividade06.ts
@@ -1,7 +1,36 @@
 import leia from 'readline-sync'
 
 export default function atividade06() {
+    Produto : [] = [];
+  
+let Produto {
+    nome: 
+    
+  }
+  
+  
+  function cadastrarProdutos(): 
+    
+    totalPreco = 0;
+  
+    
+    const n = parseInt(prompt("Quantos produtos deseja cadastrar?") || "0");
+  
+    
+    if (n <= 0) {
+      console.log("Número de produtos inválido!");
+      return;
+    }
+  
+    for (let i = 0; i < n; i++) {}
+    
+  
+}
+  
+  
+
+
 
  
-}
+
 console.log('Atividade 06')
